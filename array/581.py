7e4a4b86193e0e3800327bcbfd856a317c164da7
금교영 <keumky2@woowahan.com>
2023-01-16 01:04:39 +0900

    feat: 581문제 다시 품

--- array/581.py
+++ array/581.py
@@ -38,3 +38,45 @@ class Solution:
 nums = [2,6,4,8,10,9,15]
 findUnsortedSubarray(nums)
         
+
+MAX_NUM = 10**4 +1
+MIN_NUM = -(10**4) -1
+
+# 망한 알고리즘 실패하는 코드들... 2023년 1월 16일 어찌 퇴보하는것 같다? 
+# 교훈 최대최소 구할 때는 무조건 min, max를 쓰자. 범위를 줄이면된다. 
+class Solution:
+    def findUnsortedSubarray(self, nums: List[int]) -> int:
+            left,right = 0,0
+            length = len(nums)
+            llmp = nums[0] if nums[0] > nums[1] else MIN_NUM 
+            slmp = nums[length-1] if nums[length -1 ]< nums[length-2]  else MAX_NUM
+
+            for i in range(1,len(nums)-1): 
+
+                if ( nums[i-1] < nums[i]) and nums[i] > nums[i+1]:
+                    llmp = max(llmp, nums[i])
+                if nums[i-1] > nums[i] and nums[i] < nums[i+1]:
+                    slmp = min(slmp, nums[i])
+
+            if llmp == MIN_NUM and slmp == MAX_NUM : 
+                return 0;
+
+            for i in range(length-1,-1,-1):
+                    if  nums[i] < llmp  : 
+                        right = i
+                        break
+                    
+           
+            for i in range(length):
+                    if nums[i] > slmp :
+                        left = i
+                        break
+            return right-left+1
+
+
+                
+
+
+
+            
+
